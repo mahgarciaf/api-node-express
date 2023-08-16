fa8072e39defe116d8c99b80d15cb174453f8d1c
Marielle <marielle.gfpaulino@gmail.com>
2023-08-15 22:24:30 -0300

    acessando o banco através do model - obs deu erro

--- src/controllers/user/createUser.js
+++ src/controllers/user/createUser.js
@@ -1,11 +1,21 @@
-const createUser = (req, res) => {
-    // pegar os dados / create
-    const dados = req.body
-    console.log(dados.nome)
-    
-    res.json({
-        message: "Usuário criado com sucesso",
-        dados: dados})
+import user from '../../models/userModels.js'
+
+const createUser = async (req, res) => {
+    try {
+        const user = req.body
+        const res = await user.create(user)
+        console.log(res)
+        const [result] = resif(result.affectedRows === 1) {
+            res.status(201).json({message: 'User Created',
+                user:{
+                    id: result.
+                }
+            })
+        }
+    } catch (error) {
+        console.error(err)
+        res.status(500).json({message: 'Server error'})
+    }
 }
 
 export default createUser
--- src/controllers/user/listUsers.js
+++ src/controllers/user/listUsers.js
@@ -1,10 +1,19 @@
-const listUsers = (req, res) => {
-    const users = [
-        {id: 1, name: "João"},
-        {id: 1, name: "Maria"},
-        {id: 1, name: "José"}
-    ]
-    res.json(users)
+import user from '../../models/userModels.js'
+
+const listUsers = async (req, res) => {
+    try {
+        const result = await user.list()
+        const [rows, fields] = result
+
+        if (rows.length === 0) {
+            res.status(404).json({message: 'Users not found'})
+        } else {
+            res.json(rows)
+        }
+    } catch (error) {
+        console.error(err)
+        res.status(500).json({message: 'Server error'})
+    }
 }
 
 export default listUsers
